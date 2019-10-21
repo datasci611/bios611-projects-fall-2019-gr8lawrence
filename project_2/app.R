55af20de8bcd2786e8dd3ba21147627bb47f58b6
Tianyi Liu <gr8lawrence@tianyis-mbp.wireless-1x.unc.edu>
2019-10-21 14:07:41 -0400

    First draft of project 2

--- project_2/app.R
+++ project_2/app.R
@@ -1,1 +1,79 @@
 library(shinydashboard)
+source("helper_function.R")
+
+# Define UI for app that allows users to select variables and draw violin plots----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("UMD Data Inspection Dashboard"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Radio buttons for selecting the x-axis variable ----
+      radioButtons(inputId = "Variable",
+                   label = "Variable to View:",
+                   choices = c("Dates" = "Date", 
+                               "Number of Bus Tickets" = "Bus.Tickets..Number.of.", 
+                               "Food Provided For" = "Food.Provided.for",
+                               "Food Distributed in Pounds" = "Food.Pounds" ,
+                               "Clothing Items Distributed" = "Clothing.Items",
+                               "Diapers Distributed" = "Diapers" ,
+                               "School Kits Distributed" = "School.Kits",
+                               "Hygiene Kits Distributed" = "Hygiene.Kits",
+                               "Financial Support" = "Financial.Support"),),
+      
+      # Input: Users are allowed to select whether to include zeroes in the numeric values
+      radioButtons(inputId = "IncludeZeroes",
+                   label = "Include 0's ? (Numerical Variables Only)",
+                   choices = c("Yes" = TRUE,
+                               "No" = FALSE)),
+      
+      # Output: the text output
+      textOutput(outputId = "explanations")
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output 1: The summary table
+      tableOutput(outputId = "summaryTable"),
+      
+      # Output 2: The boxplots for summarizing numeric variables by month
+      plotOutput(outputId = "byMonthPlot")
+      
+    )
+  )
+)
+
+# Define server logic required to show the outputs ----
+server <- function(input, output) {
+  
+  # Create the table output to summarize the data
+  output$summaryTable <- renderTable({
+    if (input$Variable != "Date") {
+      make_summary(UMD_dat[[input$Variable]], include_zeroes = input$IncludeZeroes)
+    } else {
+      date_summary(UMD_dat)
+    } 
+  })
+  
+  output$byMonthPlot <- renderPlot({
+    if (input$Variable != "Date") {
+      by_month_plot(UMD_dat, input$Variable)
+    } else {
+      date_histogram(UMD_dat)
+    }
+  })
+  
+  # Create a text output to show the explanation from the metadata
+  output$explanations <- renderText({
+    paste("Explanation of the variable:", UMD_metadat[[input$Variable]])
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
--- project_2/helper_function.R
+++ project_2/helper_function.R
@@ -1,1 +1,106 @@
 library(tidyverse)
+
+## Data directory
+data_dir <- "/Users/gr8lawrence/Desktop/Bios_611/bios611-projects-fall-2019-gr8lawrence/project_2/data/"
+
+## Read data
+# UMD data
+UMD_dat <- as_tibble(read.csv(file = paste(data_dir, "UMD_Services_Provided_20190719.tsv", sep = ""), 
+                          sep = "\t", 
+                          header = TRUE))
+
+# Create the metadata
+UMD_metadat <- tibble(Date = "Date service was provided",
+                      Food.Provided.for = "Number of people in the family for which food was provided
+",
+                      Food.Pounds = "",
+                      Clothing.Items = "",
+                      School.Kits = "",
+                      Hygiene.Kits = "",
+                      Financial.Support = "Money provided to clients. Service discontinued.")
+
+# Filtering the data
+UMD_dat$Date <- as.Date(UMD_dat$Date, "%m/%d/%Y")
+# numeric_dat <- UMD_dat %>% select_if(is.numeric)  
+
+## Helper functions for creating outputs for each module
+# Filtering out 0's from data based on the user selection
+zero_filtering <- function(numeric_values, include_zeroes) {
+  if (include_zeroes) {
+    numeric_values = numeric_values %>% na.omit()
+  } else {
+    numeric_values1 = numeric_values %>% na.omit()
+    numeric_values = numeric_values1[which(numeric_values1 != 0)]
+  }
+  return(numeric_values)
+}
+# Making summary statistics
+
+
+make_summary <- function(numeric_values, include_zeroes) {
+  numeric_values <- zero_filtering(numeric_values, include_zeroes)
+  # Write the summary statistics into a table
+  df <- tibble(Total = sum(numeric_values),
+               Mean = mean(numeric_values),
+               Variance = var(numeric_values),
+               Min = min(numeric_values),
+               Max = max(numeric_values),
+               Q1 = quantile(numeric_values)[[1]],
+               Q2 = quantile(numeric_values)[[2]],
+               Q3 = quantile(numeric_values)[[3]])
+  return(df)
+}
+# Summary table for months
+date_summary <- function(df) {
+  df = df %>% 
+    mutate(Month = factor(months(Date), levels = c("January", "February", "March",
+                                                   "April", "May", "June", "July",
+                                                   "August", "September", "October",
+                                                   "November", "December"))) %>%
+    select(Month) %>%
+    group_by(Month) %>%
+    summarise(Count = n())
+  
+}
+
+date_histogram <- function(df) {
+  date_summary_df = date_summary(df) 
+  q = ggplot(date_summary_df, aes(x = Month, y = Count, fill = Month)) +
+    geom_col(alpha = 0.6) + 
+    theme_classic() +
+    labs(title = paste("Histograms of Total Number of Service Records By Month"),
+         x = "Month",
+         y = "Count of Records") +
+    scale_fill_discrete(name = "Month") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  return(q)
+}
+
+# Summarize the median by month
+# The data frame must have a column of time stamps named "Date"
+by_month_plot <- function(df, variable) {
+  df = df %>%
+    mutate(month = factor(months(Date), levels = c("January", "February", "March",
+                                                   "April", "May", "June", "July",
+                                                   "August", "September", "October",
+                                                   "November", "December"))) %>%
+    select(month, variable) %>%
+    drop_na(variable)
+
+  q = ggplot(df, aes(x = month, y = get(variable), fill = month)) +
+    geom_boxplot(alpha = 0.6) +
+    theme_classic() +
+    labs(title = paste("Boxplots of", variable, "by Month"),
+         x = "Month",
+         y = variable) +
+    scale_fill_discrete(name = "Month") +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+  return(q)
+}
+
+
+
+
+
+
